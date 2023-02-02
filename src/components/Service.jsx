226133a5ee0b42b1b13246d4f2fa757ba600544c
yeedme <73169342+yeedme@users.noreply.github.com>
2023-02-02 21:12:42 +0800

    add:serverPage

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -9,8 +9,23 @@ export default function Service() {
             <div className="text-5xl text-center">About</div>
             <div className="text-2xl text-center">what i do</div>
           </div>
-          <div className="w-80vw h-60vh flex justify-between flex-warp ">
-            <div className="md:h-40vh md:w-18vw w-35vw h-30vh  md:bg-gray-100 rounded-xl p-8 flex-shrink-0 flex items-end">
+          <div className="md:w-80vw w-90vw h-60vh flex justify-between flex-wrap bg-red-100">
+
+             <div className="md:h-40vh md:w-18vw w-40vw h-30vh  md:bg-gray-100 bg-white rounded-xl p-8 flex-shrink-0 flex items-end">
+              <div className="h-25vh w-full">
+                <div className="w-5vw h-5vw bg-red-100">
+                
+                </div>
+                <div className=" h-5vw bg-red-100">
+                  <h2 className="text-lg">web</h2>
+                  <h2 className="text-lg">Designer</h2>
+                </div>
+                <div className="h-5vw">
+                View More -
+              </div>
+              </div>     
+            </div>
+            <div className="md:h-40vh md:w-18vw w-40vw h-30vh  md:bg-gray-100 bg-white rounded-xl p-8 flex-shrink-0 flex items-end">
               <div className="h-25vh w-full">
                 <div className="w-5vw h-5vw bg-red-100">
                 
@@ -24,7 +39,7 @@ export default function Service() {
               </div>
               </div>     
             </div>
-            <div className="md:h-40vh md:w-18vw w-35vw h-30vh  md:bg-gray-100 rounded-xl p-8 flex-shrink-0 flex items-end">
+            <div className="md:h-40vh md:w-18vw w-40vw h-30vh  md:bg-gray-100 bg-white rounded-xl p-8 flex-shrink-0 flex items-end">
               <div className="h-25vh w-full">
                 <div className="w-5vw h-5vw bg-red-100">
                 
@@ -37,21 +52,7 @@ export default function Service() {
                 View More ->
               </div>
               </div>     
-            </div>
-            <div className="md:h-40vh md:w-18vw w-35vw h-30vh  md:bg-gray-100 rounded-xl p-8 flex-shrink-0 flex items-end">
-              <div className="h-25vh w-full">
-                <div className="w-5vw h-5vw bg-red-100">
-                
-                </div>
-                <div className=" h-5vw bg-red-100">
-                  <h2 className="text-lg">web</h2>
-                  <h2 className="text-lg">Designer</h2>
-                </div>
-                <div className="h-5vw">
-                View More ->
-              </div>
-              </div>     
-            </div>
+            </div> 
             
             {/* <div className="md:h-40vh md:w-18vw bg-gray-100 rounded-xl p-8 "></div>
             <div className="md:h-40vh md:w-18vw bg-gray-100 rounded-xl p-8 "></div> */}
